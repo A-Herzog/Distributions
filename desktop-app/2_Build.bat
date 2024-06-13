6bf3f348952a690997ce6d32e47b7ec31acfcf45
Alexander Herzog <alexander.herzog@gmx.de>
2024-06-13 13:28:08 +0200

    Updated desktop app version

--- desktop-app/2_Build.bat
+++ desktop-app/2_Build.bat
@@ -1,8 +1,11 @@
 cd ..
 del Distributions.exe
+del Distributions_Linux_MacOS.zip
 call neu.cmd build --release
 cd desktop-app
 "C:\Program Files (x86)\NSIS\makensis.exe" Launcher.nsi
 move Distributions.exe ..
 cd ..
+move .\dist\Distributions-release.zip Distributions_Linux_MacOS.zip
 rmdir /S /Q dist
+cd desktop-app
